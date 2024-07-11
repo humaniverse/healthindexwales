ce1d06588d910f2bc799858269d65e5ae2487beb
jennajt <jennajltan@gmail.com>
2024-07-11 08:06:32 +0100

    Turned repo into package and included test files

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @importFrom tibble tibble
+NULL
+
+#' DF - test documentation - to be deleted
+#'
+#' A dataset ..
+#'
+#' @format A data frame with .. rows and . variables:
+#' \describe{
+#'   \item{X1}{...}
+#'   \item{X1.1}{..}
+#'   ...
+#' }
+#' @source \url{https://www..../}
+"dftest"
