d572174223e58919c27ce599f0c938745629422f
Sophie Hammond <sophieclhammond@gmail.com>
2024-07-16 09:53:39 +0100

    Created physical activity dataset, added documentation

--- R/data.R
+++ R/data.R
@@ -41,3 +41,17 @@ NULL
 #' }
   #' @source \url{https://fieldsintrust.org/insights/green-space-index}
 "hp_greenspace_access"
+
+#' Physical activity Wales 2021-2023
+#'
+#' Dataset containing information about the percentage of physically active adults in Wales
+#'
+#' @format A data frame with 22 rows and 3 variables:
+#' \describe{
+#'   \item{ltla21_code}{Area code}
+#'   \item{Percent adults active at least 150 minutes last week}{Percentage of people aged 16 and over in each local authority claiming to have exercised for at least 150 minutes the previous week, age standardised}
+#'   \item{Year}{Time frame}
+#'.  ...
+#' }
+#' @source \url{https://statswales.gov.wales/Catalogue/National-Survey-for-Wales/Population-Health/Adult-Lifestyles}
+"hl_physical_activity"
---
+++ data-raw/healthy-lives/behavioural-risk-factors/physical-activity.R
@@ -0,0 +1,23 @@
+# ---- Load packages ----
+library(tidyverse)
+library(statswalesr)
+
+# ---- Scrape data from stats wales and save as tempfile ----
+# ---- Source:https://statswales.gov.wales/Catalogue/National-Survey-for-Wales/Population-Health/Adult-Lifestyles ----
+df <- statswales_get_dataset("hlth5072")
+
+# ---- Clean data ----
+# ---- Filtered dataset includes percentage of adults (age 16+) who claim to have had ≥150 minutes of physical activity the previous week, age standardised ----
+# ---- For Year, data from 2021-22 & 2022-23 has been combined
+hl_physical_activity <- df |>
+  filter(str_starts(Variable_ItemName_ENG, "Active at") &
+           str_starts(Measure_ItemName_ENG, "Per") &
+           str_starts(Year_SortOrder, "14") &
+           str_starts(Standardisation_ItemName_ENG, "A") &
+           str_starts(Area_Code, "W0")) |>
+  select(ltla21_code = Area_Code,
+         `Percent adults active at least 150 minutes last week` = Data,
+         Year = Year_ItemName_ENG)
+
+# ---- Save output to data/ folder ----
+usethis::use_data(hl_physical_activity, overwrite = TRUE)
