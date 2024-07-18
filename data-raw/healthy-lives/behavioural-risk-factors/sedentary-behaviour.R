dd7534afe77205d20da294be53cdc108f639fbeb
Sophie Hammond <sophieclhammond@gmail.com>
2024-07-18 09:28:16 +0100

    Made changes to script, updated metadata

--- data-raw/healthy-lives/behavioural-risk-factors/sedentary-behaviour.R
+++ data-raw/healthy-lives/behavioural-risk-factors/sedentary-behaviour.R
@@ -2,13 +2,13 @@
 library(tidyverse)
 library(statswalesr)
 
-# ---- Scrape data from stats wales and save as tempfile ----
-# ---- Source:https://statswales.gov.wales/Catalogue/National-Survey-for-Wales/Population-Health/Adult-Lifestyles ----
+# ---- Scrape data from stats wales ----
+#Source:https://statswales.gov.wales/Catalogue/National-Survey-for-Wales/Population-Health/Adult-Lifestyles
 df <- statswales_get_dataset("hlth5072")
 
 # ---- Clean data ----
-# ---- Filtered dataset includes percentage of adults (age 16+) who claim to have had <30 minutes of physical activity the previous week, age standardised ----
-# ---- For Year, data from 2021-22 & 2022-23 has been combined
+#Filtered dataset includes percentage of adults (age 16+) who claim to have had <30 minutes of physical activity the previous week, age standardised
+#For Year, data from 2021-22 & 2022-23 has been combined
 hl_sedentary_behaviour <- df |>
   filter(str_starts(Variable_ItemName_ENG, "Active less") &
            str_starts(Measure_ItemName_ENG, "Per") &
