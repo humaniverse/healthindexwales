692306d59fe194ecf01b891b5127869ba1a221f8
Sophie Hammond <sophieclhammond@gmail.com>
2024-07-25 10:59:36 +0100

    Created early years development dataset, added documentation, updated metadata

--- R/data.R
+++ R/data.R
@@ -41,3 +41,18 @@ NULL
 #' }
   #' @source \url{https://fieldsintrust.org/insights/green-space-index}
 "hp_greenspace_access"
+
+
+#' Early Years Development Wales 2021-2023
+#'
+#' Dataset containing information about the percentage of students in Wales achieving expected level at foundation phase
+#'
+#' @format A data frame with 22 rows and 3 variables:
+#' \describe{
+#'   \item{ltla21_code}{Area code}
+#'   \item{Percent pupils achieving expected level across four foundation level tested areas}{Percentage of foundation phase students, aged 7, who achieve the expected level, level 5, in all four areas of the foundation phase tests per local authority. The four areas are 1) Personal and social development, well-being and cultural diversity, 2) Language, literacy and communication skills - English, 3) Language, literacy and communication skills - Welsh, 4) Mathematical development}
+#'   \item{Year}{Time frame}
+#'.  ...
+#' }
+#' @source \url{https://statswales.gov.wales/Catalogue/Education-and-Skills/Schools-and-Teachers/Examinations-and-Assessments/Foundation-Phase}
+"hl_early_years_development"
---
+++ data-raw/healthy-lives/children-and-young-people/early_years_development.R
@@ -0,0 +1,30 @@
+# ---- Load packages ----
+library(tidyverse)
+library(statswalesr)
+
+# ---- Scrape data from stats wales ----
+#Source:https://statswales.gov.wales/Catalogue/Education-and-Skills/Schools-and-Teachers/Examinations-and-Assessments/Foundation-Phase 
+df <- statswales_get_dataset("SCHS0153")
+
+# ---- Clean data ----
+#Dataset includes percentage of students at foundation phase, aged 3-7, who achieved the expected outcome level or above (level 5) across the four tested areas
+#Tested areas were: 1) Personal and social development, well-being and cultural diversity, 2) Language, literacy and communication skills - English, 3) Language, literacy and communication skills - Welsh, 4) Mathematical development
+hl_early_years_development <- df |>
+  filter(str_starts(Description_Code, "L5") &
+           str_starts(Subject_ItemName_ENG, "F") &
+           str_starts(Gender_Code, "3") &
+           str_starts(Year_Code, "2017") &
+           str_starts(Area_AltCode1, "W06")) |>
+  select(ltla21_code = Area_AltCode1,
+         `Percent pupils achieving expected level across four foundation phase tested areas` = Data,
+         Year = Year_Code) |>
+  arrange(ltla21_code)
+
+
+# ---- Save output to data/ folder ----
+usethis::use_data(hl_early_years_development, overwrite = TRUE)
+
+
+
+
+         
