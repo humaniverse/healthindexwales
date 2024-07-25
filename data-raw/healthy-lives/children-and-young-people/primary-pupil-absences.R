270cc2ffcf5aa46675fde3a69fad8fd2b117a71d
Sophie Hammond <sophieclhammond@gmail.com>
2024-07-25 17:22:02 +0100

    Started working on primary absences dataset

---
+++ data-raw/healthy-lives/children-and-young-people/primary-pupil-absences.R
@@ -0,0 +1,32 @@
+# ---- Load packages ----
+library(tidyverse)
+library(httr)
+library(readODS)
+library(dplyr)
+library(statswalesr)
+
+# ---- Scrape primary schools URL ----
+download.file("https://www.gov.wales/sites/default/files/statistics-and-research/2024-03/absenteeism-from-primary-schools-september-2022-to-august-2023-113.ods", 
+              destfile = "absenteeism.ods")
+primary_data <- read_ods("absenteeism.ods", sheet = 10, range = "A4:J92")|>
+  filter(str_ends(Measure, "missed")) |>
+  select(`Local authority`,
+         `Primary percentage of absences` = `2022/23`
+  )
+primary_data$`Local authority`[primary_data$`Local authority` == "The Vale of Glamorgan"] <- "Vale of Glamorgan"
+
+# ---- Scrape ltla lookup file ----
+# Specify the URL
+url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/migrationwithintheuk/datasets/userinformationenglandandwaleslocalauthoritytoregionlookup/june2020/lasregionew2021lookup.xlsx"
+
+# Download the file to a temporary location
+temp_file <- tempfile(fileext = ".xlsx")
+download.file(url, temp_file, mode = "wb")
+code_lookup <- read_excel(temp_file, range = "A5:D366")|>
+  filter(str_starts(`LA code`, "W0")) 
+
+# ---- Join ltla lookup to absence dataset ----
+absence_data <- left_join(primary_data, code_lookup, by = c("Local authority" = "LA name") |>
+                            select(ltla21_code = `LA code`,
+                                    `Primary percentage of absences`)
+)
