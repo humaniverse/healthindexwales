c3d695bbc885232974720aa3736561c6cddc30b1
Sophie Hammond <sophieclhammond@gmail.com>
2024-07-29 17:17:43 +0100

    Added comments

--- data-raw/healthy-lives/behavioural-risk-factors/smoking.R
+++ data-raw/healthy-lives/behavioural-risk-factors/smoking.R
@@ -11,10 +11,10 @@ df <- statswales_get_dataset("hlth5072")
 #For Year, data from 2021-22 & 2022-23 has been combined
 hl_smoking <- df |>
   filter(str_starts(Variable_ItemName_ENG, "Smoker") &
-           str_starts(Measure_ItemName_ENG, "Per") &
-           str_starts(Year_SortOrder, "14") &
-           str_starts(Standardisation_ItemName_ENG, "A") &
-           str_starts(Area_Code, "W0")) |>
+           str_starts(Measure_ItemName_ENG, "Percentage") & #Filters column to include only percentage of adults, excludes data on sample and confidence intervals
+           str_starts(Year_SortOrder, "14") & #Filters column to only include 2021-22 and 2022-23
+           str_starts(Standardisation_ItemName_ENG, "Age standardised") &
+           str_starts(Area_Code, "W0")) |> #Filters column to only include local authority codes
   select(ltla21_code = Area_Code,
          `Percentage smokers` = Data,
          Year = Year_ItemName_ENG)
