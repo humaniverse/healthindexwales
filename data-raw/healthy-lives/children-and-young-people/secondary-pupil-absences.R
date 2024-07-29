2fe3ac8dd70320d2beada310d3d4d97f6520f496
Sophie Hammond <sophieclhammond@gmail.com>
2024-07-29 13:39:37 +0100

    make changes to script

--- data-raw/healthy-lives/children-and-young-people/secondary-pupil-absences.R
+++ data-raw/healthy-lives/children-and-young-people/secondary-pupil-absences.R
@@ -3,20 +3,26 @@ library(tidyverse)
 library(httr)
 library(readODS)
 library(dplyr)
-library(statswalesr)
 
 # ---- Scrape secondary schools URL ----
+#Source:https://www.gov.wales/absenteeism-secondary-schools
 download.file("https://www.gov.wales/sites/default/files/statistics-and-research/2023-10/absenteeism-from-secondary-schools-september-2022-to-august-2023-revised-936.ods", 
               destfile = "absenteeism.ods")
+
+# ---- Clean primary data ----
 secondary_data <- read_ods("absenteeism.ods", sheet = 10, range = "A4:J92") |>
   filter(str_ends(Indicator, "missed")) |>
   select(`Local authority`,
          `Secondary percentage of absences` = `2022/23 [r]`
   )
+
+#Rename Vale of Glamorgan and Cardiff so it works for join
+secondary_data$`Local authority`[secondary_data$`Local authority` == "The Vale of Glamorgan"] <- "Vale of Glamorgan"
 secondary_data$`Local authority`[secondary_data$`Local authority` == "Cardiff [r]"] <- "Cardiff"
 
 # ---- Scrape ltla lookup file ----
 # Specify the URL
+#Source:https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/migrationwithintheuk/datasets/userinformationenglandandwaleslocalauthoritytoregionlookup
 url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/migrationwithintheuk/datasets/userinformationenglandandwaleslocalauthoritytoregionlookup/june2020/lasregionew2021lookup.xlsx"
 
 # Download the file to a temporary location
@@ -26,4 +32,14 @@ code_lookup <- read_excel(temp_file, range = "A5:D366")|>
   filter(str_starts(`LA code`, "W0")) 
 
 # ---- Join ltla lookup to absence dataset ----
-absence_data <- left_join(secondary_data, code_lookup, by = c("Local authority" = "LA name"))
+joined_data <- left_join(secondary_data, code_lookup, by = c("Local authority" = "LA name"))
+
+# ---- Clean joined dataset ----
+hl_secondary_pupil_absences <- joined_data |>
+  select(ltla21_code = `LA code`,
+         `Secondary percentage of absences`) |>
+  mutate(Year = "2022/23") |>
+  arrange(ltla21_code)
+
+# ---- Save output to data/ folder ----
+usethis::use_data(hl_secondary_pupil_absences, overwrite = TRUE) 
