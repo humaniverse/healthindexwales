9a43f5eaf3144d6215101dfc0c1751165e1fb31d
zaramorganoluwole <zaramorganoluwole@gmail.com>
2024-08-05 13:42:37 +0100

    updated reception weight script

--- data-raw/healthy-lives/physiological-risk-factors/reception_overweight_obese.R
+++ data-raw/healthy-lives/physiological-risk-factors/reception_overweight_obese.R
@@ -1,17 +1,26 @@
 # ---- Load necessary libraries ----
 library(tidyverse)
-library(devtools)
 library(readxl)
+library(devtools)
 
-# ---- Scraping data from excel ----
+# ---- Define paths and URLs ----
 # Source: https://phw.nhs.wales/services-and-teams/child-measurement-programme/cmp-2022-23/
+# File path for Child Measurement Programme Data 2022-2023 (CMP)
+local_cmp_file_path <- "C:/Users/ZaraMorgan/Downloads/2. CMP_Data_2022_2023.xlsx"
 
-# ---- Create a temporary file path ----
-temp_file <- tempfile(fileext = ".xlsx")
+# URL for the LTLA lookup file
+ltla_url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/migrationwithintheuk/datasets/userinformationenglandandwaleslocalauthoritytoregionlookup/june2020/lasregionew2021lookup.xlsx"
 
-# ---- Copying excel file to temporary file ----
-# Copy the original file to the temporary file to avoid modifying the original and for accessibility
-file.copy("C:/Users/ZaraMorgan/Downloads/2. CMP_Data_2022_2023.xlsx", temp_file, overwrite = TRUE)
+# ---- Create a temporary file path for LTLA lookup ----
+temp_ltla_file <- tempfile(fileext = ".xlsx")
+
+# ---- Download the LTLA lookup file ----
+download.file(ltla_url, temp_ltla_file, mode = "wb")
+
+# ---- Read and filter the LTLA lookup file ----
+code_lookup <- read_excel(temp_ltla_file, range = "A5:D366") |>
+  filter(str_starts(`LA code`, "W0")) |>
+  select(`LA code`, `LA name`)
 
 # ---- Define unwanted geographies ----
 unwanted_geographies <- c(
@@ -21,27 +30,14 @@ unwanted_geographies <- c(
   "Cardiff and Vale UHB", "Hywel Dda UHB", "Aneurin Bevan UHB"
 )
 
-# ---- Reading and filtering Excel file ----
-X2_CMP_Data_2022_2023 <- read_excel(temp_file, sheet = "3b", skip = 3) |>
+# ---- Reading and filtering the CMP Data Excel file ----
+# Read the CMP Data Excel file
+child_weight_data_2022_2023 <- read_excel(local_cmp_file_path, sheet = "3b", skip = 3) |>
   filter(!Geography %in% unwanted_geographies) |>
-  mutate(Geography = recode(Geography, "Powys THB" = "Powys")) 
-
-# ---- Scrape LTLA lookup file ----
-# Specify the URL
-# Source: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/migrationwithintheuk/datasets/userinformationenglandandwaleslocalauthoritytoregionlookup
-url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/migrationwithintheuk/datasets/userinformationenglandandwaleslocalauthoritytoregionlookup/june2020/lasregionew2021lookup.xlsx"
-
-# ---- Download the file to a temporary location ----
-temp_file <- tempfile(fileext = ".xlsx")
-download.file(url, temp_file, mode = "wb")
-
-# ---- Only include relevant columns ----
-code_lookup <- read_excel(temp_file, range = "A5:D366") |>
-  filter(str_starts(`LA code`, "W0")) |>
-  select(`LA code`, `LA name`)
-
-# ---- Merging X2_CMP_Data_2022_2023 with code_lookup ----
-hl_reception_overweight_obese <- X2_CMP_Data_2022_2023 |>
+  mutate(Geography = recode(Geography, "Powys THB" = "Powys"))
+  
+  # ---- Merging X2_CMP_Data_2022_2023 with code_lookup ----
+hl_reception_overweight_obese <- child_weight_data_2022_2023 |>
   left_join(code_lookup, by = c("Geography" = "LA name")) |>
   mutate(Year = "2022-2023") |>
   select(
@@ -49,6 +45,5 @@ hl_reception_overweight_obese <- X2_CMP_Data_2022_2023 |>
     percentage_overweight_obese = `91st centile and above (%)`,
     Year
   )
-
-# ---- Saving to data folder using usethis:: function ----
-usethis::use_data(hl_reception_overweight_obese, overwrite = TRUE)
+  # ---- Saving to data/ folder using usethis:: ----
+  usethis::use_data(hl_reception_overweight_obese, overwrite = TRUE)
