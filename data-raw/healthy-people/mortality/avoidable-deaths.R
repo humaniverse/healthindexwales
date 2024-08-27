9920c85c93db504fc2afbddb281575798df899dd
Sophie Hammond <sophieclhammond@gmail.com>
2024-08-27 12:10:02 +0100

    Refactored mortality scripts

--- data-raw/healthy-people/mortality/avoidable-deaths.R
+++ data-raw/healthy-people/mortality/avoidable-deaths.R
@@ -1,38 +1,40 @@
-# ---- Load libraries ----
+# ---- Load packages ----
 library(tidyverse)
 library(httr)
 library(readxl)
 library(geographr)
 library(sf)
 
+# ---- Load functions from utils.R ----
 source("R/utils.R")
 
+# ---- Load data ----
+# Load Welsh ltla codes and names from geographr 
 wales_lookup <-
-  boundaries_lad %>%
-  as_tibble() %>%
-  select(starts_with("lad")) %>%
-  filter_codes(lad_code, "^W")
+  boundaries_ltla21 |>
+  as_tibble() |>
+  select(starts_with("ltla21")) |>
+  filter_codes(ltla21_code, "^W")
 
-# ---- Retrieve data ----
+# Scrape URL and save dataset as tempfile
 GET(
   "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fhealthandsocialcare%2fcausesofdeath%2fdatasets%2favoidablemortalitybylocalauthorityinenglandandwales%2f2019/avoidablemortalitybylocalauthority2019.xls",
   write_disk(tf <- tempfile(fileext = ".xls"))
 )
 
-raw <-
+# ---- Clean data ----
+hpe_avoidable_deaths <-
   read_excel(
     tf,
     sheet = "Table 1 ",
     range = "B5:BS435"
-  )
-
-avoidable_deaths <-
-  raw %>%
+  ) |>
   select(
-    lad_code = `...1`,
+    ltla21_code = `...1`,
     avoidable_deaths_per_100000 = `Rate per 100,000 population...67`
-  ) %>%
-  right_join(wales_lookup) %>%
-  select(-lad_name)
+  ) |>
+  right_join(wales_lookup) |>
+  select(-ltla21_name)
 
-write_rds(avoidable_deaths, "data/vulnerability/health-inequalities/wales/healthy-people/avoidable-deaths.rds")
+# ---- Save output to data/ folder ----
+usethis::use_data(hpe_avoidable_deaths, overwrite = TRUE)
