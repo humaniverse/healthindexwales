c41e3490d02c7f8663c3ec26a87e35a2051d43f6
CatReid <CatrionaReid@redcross.org.uk>
2024-09-17 10:43:09 +0100

    upload and filter data sets
    uploaded and filtered population and rough sleepers data sets

--- data-raw/healthy-places/living-conditions/rough-sleeping.R
+++ data-raw/healthy-places/living-conditions/rough-sleeping.R
@@ -1,12 +1,9 @@
 # Installing relevant packages
 library(tidyverse)
-library(healthindexwales)
 library(readxl)
 library(httr)
 
 # Extracting relevant data sets from statswalesr
-install.packages("statswalesr")
-devtools::install_github("jamie-ralph/statswalesr")
 library(statswalesr)
 rough_sleepers <- statswales_search(c("rough sleepers"))
 glimpse(rough_sleepers)
@@ -25,5 +22,39 @@ statswales_search(c("homelessness"))
 
 
 # Scrape data from stats Wales
-# Source:https://statswales.gov.wales/Catalogue/Housing/Homelessness/homelessness-accommodation-provision-and-rough-sleeping/roughsleepers-by-localauthority
+# Source:https://statswales.gov.wales/v/P2SD
 df <- statswales_get_dataset("HOUS0454")
+
+# Scrape URL
+# Source:https://statswales.gov.wales/v/P2SD
+url <- "https://statswales.gov.wales/v/P2SD"
+
+# Download and read URL as temp file
+temp_file <- tempfile(fileext = ".xlsx")
+download.file(url, temp_file, mode = "wb")
+df <- read_excel(temp_file)
+df <- read.csv(temp_file)
+
+# Upload csv files for rough sleepers and population data
+read.csv()
+df <- rough_sleepers_data
+head(population_df)
+df <- rough_sleepers_data
+
+# Need to filter to only include Welsh data (mid-2022 to mid-2023)
+population_df <- population_df |>
+  filter(location == "Wales")
+str(population_df)
+head(population_df)
+
+population_df_wales <- population_df |>
+  select(`...4`, `...5`, `...6`) |>
+  slice(12:33)
+
+# Now need to filter to only include data from June 2023 - June 2024
+head(rough_sleepers_data)
+rough_sleepers_data <- rough_sleepers_data |>
+  select(`Housing > Homelessness > Homelessness accommodation provision and rough sleeping > Rough sleepers by local authority`, `...4`:`...16`) |>
+  slice(4:25)
+
+# Merge data sets to then calculate the % of people in each LA who are rough sleepers
