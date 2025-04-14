5e79444ef2b7e949c7080c32c74ac27aee4a55a1
CatReid <CatrionaReid@redcross.org.uk>
2025-04-14 12:06:26 +0100

    Removed personal-crime and low-level-crime
    Data OOD and found better source

--- R/data.R
+++ R/data.R
@@ -72,53 +72,6 @@ NULL
 "hl_teenage_pregnancy"
 
 
-#' Low-Level Crime Scores for Wales
-#'
-#' A dataset containing low-level crime scores for local authorities in Wales.
-#' The low-level crime score is calculated as the sum of bicycle theft and shoplifting.
-#'
-#' @description
-#' Note: The combined authority for Cwm Taf has been split into its respective
-#' local authorities, Merthyr Tydfil and Rhondda Cynon Taf. This adjustment was made
-#' because the data for Cwm Taf was originally documented as a combined local authority.
-#' The data values for rows 18 and 19, corresponding to these local authorities,
-#' remain unchanged.
-#'
-#' @format A data frame with the following columns:
-#' \describe{
-#'   \item{local_authority_code}{Character. The code for the local authority.}
-#'   \item{local_authority_name}{Character. The name of the local authority.}
-#'   \item{low_level_crime_score}{Numeric. The personal crime score, as calculated according to Health Index England, is the sum of Bicycle Theft and Shoplifting.This score is measured per 1,000 persons, based on the ONS mid-year population estimates for the local authority region. A lower score is better as it indicates lower crime for the region.}
-#'   \item{low_level_crime_per_1k}{Numeric. The rate of low-level crime (bicycle theft and shoplifting) per 1,000 population, based on the ONS mid-year population estimates for the local authority region. A lower rate is better as it indicates lower crime for the region.}
-#'   \item{year}{Year. The year relevant to the data snapshot.}
-#' }
-#' @source \url{https://www.ons.gov.uk/} and \url{https://opendata.arcgis.com/}
-"hp_low_level_crimes"
-
-#' Personal Crime Scores for Wales
-#'
-#' A dataset containing personal crime scores for local authorities in Wales.
-#' The personal crime score is calculated as the sum of violence against the person,
-#' sexual offences, and robbery.
-#'
-#' @description
-#' Note: The combined authority for Cwm Taf has been split into its respective
-#' local authorities, Merthyr Tydfil and Rhondda Cynon Taf. This adjustment was made
-#' because the data for Cwm Taf was originally documented as a combined local authority.
-#' The data values for rows 18 and 19, corresponding to these local authorities,
-#' remain unchanged.
-#'
-#' @format A data frame with the following columns:
-#' \describe{
-#'   \item{local_authority_code}{Character. The code for the local authority.}
-#'   \item{local_authority_name}{Character. The name of the local authority.}
-#'   \item{personal_crime_per_1k}{Numeric. The rate of personal crime (violence against the person, sexual offences, robbery, theft against the person, criminal damage and arson) per 1,000 population, based on the ONS mid-year population estimates for the local authority region. A lower rate is better as it indicates lower crime for the region.}
-#'   \item{year}{Year. The year relevant to the data snapshot}
-#' }
-#' @source \url{https://www.ons.gov.uk/} and \url{https://opendata.arcgis.com/}
-"hp_personal_crime"
-
-
 #' Low Birth Weight Percentage by year Wales
 #'
 #' Dataset containing information about the percentage of live births under 2500g in Wales
