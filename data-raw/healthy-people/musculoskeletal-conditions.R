97936978395e83693c9f563acb98aba8fc7ab8f4
Cat Reid <115796955+CatReid@users.noreply.github.com>
2025-04-15 16:36:45 +0100

    Update musculoskeletal-conditions.R

--- data-raw/healthy-people/musculoskeletal-conditions.R
+++ data-raw/healthy-people/musculoskeletal-conditions.R
@@ -7,7 +7,7 @@ library(geographr)
 wales_hb_ltla <- lookup_ltla21_lhb22
 
 
-# Respiratory Conditions data
+# Musculoskeletal Conditions data
 # Source: https://statswales.gov.wales/Catalogue/Health-and-Social-Care/NHS-Primary-and-Community-Activity/GMS-Contract/diseaseregisters-by-localhealthboard
 
 temp_zip <- tempfile(fileext = ".zip")
