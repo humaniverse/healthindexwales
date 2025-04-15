075749bf6d92e878e1ebadb39a6835481307d137
CatReid <CatrionaReid@redcross.org.uk>
2025-04-15 11:50:19 +0100

    Created low-level-crimes indicator

---
+++ data-raw/healthy-places/low-level-crime.R
@@ -0,0 +1,16 @@
+# ---- Load packages ----
+library(tidyverse)
+library(geographr)
+
+# ---- Get and clean data ----
+# Police Force Areas data
+# Source: https://geoportal.statistics.gov.uk/datasets/ons::lad-to-community-safety-partnership-to-pfa-december-2024-lookup-in-ew/explore
+
+
+
+
+# Wales LTLA and Postcode data
+
+lookup
+
+
