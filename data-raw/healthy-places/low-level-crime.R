4ce41eddba990e6667c6cd3fed5352bbfce8333b
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-04-22 16:48:36 +0200

    Format w/ air

--- data-raw/healthy-places/low-level-crime.R
+++ data-raw/healthy-places/low-level-crime.R
@@ -14,7 +14,11 @@ wales_lookup <- lookup_ltla24_csp24_pfa24 |>
 # Source: https://www.ons.gov.uk/peoplepopulationandcommunity/crimeandjustice/datasets/policeforceareadatatables
 
 tf <- tempfile(fileext = ".xlsx")
-download.file("https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/crimeandjustice/datasets/policeforceareadatatables/yearendingseptember2024/policeforceareatablesyearendingseptember2024final.xlsx", tf, mode = "wb")
+download.file(
+  "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/crimeandjustice/datasets/policeforceareadatatables/yearendingseptember2024/policeforceareatablesyearendingseptember2024final.xlsx",
+  tf,
+  mode = "wb"
+)
 
 low_level_crime_raw <- read_excel(tf, sheet = 6, skip = 7)
 
@@ -23,17 +27,25 @@ low_level_crime <- low_level_crime_raw |>
     str_starts(`Area Code`, "W"),
     `Area Code` != "W92000004"
   ) |>
-  mutate(across(c(`Total recorded crime (excluding fraud)\r\n [note 2]`:`Miscellaneous crimes against society`), as.numeric),
-         low_level_crime_rate_per_1k = rowMeans(across(c(`Bicycle theft`, `Shoplifting`)), na.rm = TRUE),
-         year = "2023-2024")
+  mutate(
+    across(
+      c(
+        `Total recorded crime (excluding fraud)\r\n [note 2]`:`Miscellaneous crimes against society`
+      ),
+      as.numeric
+    ),
+    low_level_crime_rate_per_1k = rowMeans(
+      across(c(`Bicycle theft`, `Shoplifting`)),
+      na.rm = TRUE
+    ),
+    year = "2023-2024"
+  )
 
 
 # Join datasets
 places_low_level_crime <- low_level_crime |>
   left_join(wales_lookup, by = c("Area Code" = "pfa24_code")) |>
-  select(ltla25_code = ltla24_code,
-         low_level_crime_rate_per_1k,
-         year)
+  select(ltla25_code = ltla24_code, low_level_crime_rate_per_1k, year)
 
 
 # ---- Save output to data/ folder ----
