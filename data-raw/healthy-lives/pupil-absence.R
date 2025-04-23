29e8e3e4f90dd752a7c4ee6bd00c812fc79fd0fd
CatReid <CatrionaReid@redcross.org.uk>
2025-04-23 12:32:58 +0100

    Created pupil-absence script
    Added where sources are from

---
+++ data-raw/healthy-lives/pupil-absence.R
@@ -0,0 +1,5 @@
+# Primary School Absenteeism
+# Source: https://www.gov.wales/absenteeism-primary-schools-september-2023-august-2024
+
+# Secondary School Absenteeism
+# Source: https://www.gov.wales/absenteeism-secondary-schools-september-2023-august-2024
