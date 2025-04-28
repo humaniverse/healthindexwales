633a5acef3bb5dc2d6f96610175e3592192f1e6b
CatReid <CatrionaReid@redcross.org.uk>
2025-04-28 11:06:40 +0100

    Completed pupil-absence
    Completed pupil-absence, data.R and metadata updated

--- data-raw/healthy-lives/pupil-absence.R
+++ data-raw/healthy-lives/pupil-absence.R
@@ -1,5 +1,64 @@
-# Primary School Absenteeism
+# ---- Load packages ----
+library(tidyverse)
+library(readODS)
+library(geographr)
+
+# ---- Get and clean data ----
+# Wales LTLA and HB Codes
+wales_hb_ltla <- lookup_ltla21_lhb22
+
+
+# Primary School Absenteeism data
 # Source: https://www.gov.wales/absenteeism-primary-schools-september-2023-august-2024
 
-# Secondary School Absenteeism
+primary_absenteeism_link <- "https://www.gov.wales/sites/default/files/statistics-and-research/2024-12/absenteeism-from-primary-schools-september-2023-to-august-2024-649.ods"
+tempfile_p <- tempfile(fileext = ".ods")
+download.file(primary_absenteeism_link, destfile = tempfile_p, mode = "wb")
+
+primary_absenteeism_raw <- read_ods(tempfile_p, sheet = 10, skip = 3)
+
+primary_absenteeism <- primary_absenteeism_raw |>
+  filter(Measure == "Percentage of half-day sessions missed for unauthorised absence")
+
+
+# Secondary School Absenteeism data
 # Source: https://www.gov.wales/absenteeism-secondary-schools-september-2023-august-2024
+
+secondary_absenteeism_link <- "https://www.gov.wales/sites/default/files/statistics-and-research/2024-09/absenteeism-from-secondary-schools-september-2023-to-august-2024.ods"
+tempfile_s <- tempfile(fileext = ".ods")
+download.file(secondary_absenteeism_link, destfile = tempfile_s, mode = "wb")
+
+secondary_absenteeism_raw <- read_ods(tempfile_s, sheet = 10, skip = 3)
+
+secondary_absenteeism <- secondary_absenteeism_raw |>
+  filter(Attribute == "Percentage of half-day sessions missed for unauthorised absence")
+
+
+# Join absenteeism data
+pupil_absenteeism <- primary_absenteeism |>
+  left_join(secondary_absenteeism, by = c("Local authority" = "Local authority")) |>
+  mutate(`Local authority` = str_remove(`Local authority`, "^The\\s+")) |>
+  select(`Local authority`,
+    primary_absence = `2023/24.x`,
+    secondary_absence = `2023/24.y`
+  )
+
+
+# Join absenteeism and geographic data
+lives_pupil_absence <- pupil_absenteeism |>
+  left_join(wales_hb_ltla, by = c("Local authority" = "ltla21_name")) |>
+  group_by(`Local authority`) |>
+  mutate(
+    total_pupil_absence_percentage = ((primary_absence + secondary_absence) / 2),
+    year = "2023/24"
+  ) |>
+  ungroup() |>
+  select(
+    ltla23_code = ltla21_code,
+    total_pupil_absence_percentage,
+    year
+  )
+
+
+# ---- Save output to data/ folder ----
+usethis::use_data(lives_pupil_absence, overwrite = TRUE)
