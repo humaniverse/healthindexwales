0d21c16498dea32978d2850d00a2f8c0b91bccc3
CatReid <CatrionaReid@redcross.org.uk>
2025-04-29 08:54:07 +0100

    Completed alcohol-misuse
    Completed alcohol-misuse, data.R and metadata completed

--- data-raw/healthy-lives/alcohol-misuse.R
+++ data-raw/healthy-lives/alcohol-misuse.R
@@ -14,3 +14,23 @@ wales_hb_ltla <- lookup_ltla21_lhb22
 # Source: https://phw.nhs.wales/publications/publications1/data-mining-wales-the-annual-profile-for-substance-misuse-2023-2024/
 
 alcohol_misuse_raw <- read_excel("data-raw/healthy-lives/raw-data/alcohol_misuse.xlsx")
+
+alcohol_misuse <- alcohol_misuse_raw |>
+  mutate(
+    `Local Authority` = str_remove(`Local Authority`, "^The\\s+"),
+    year = "2023/24"
+  )
+
+
+# Join datasets
+lives_alcohol_misuse <- alcohol_misuse |>
+  left_join(wales_hb_ltla, by = c("Local Authority" = "ltla21_name")) |>
+  select(
+    ltla23_code = ltla21_code,
+    alcohol_admissions_rate_per_100k = `EASR per 100,000 population 2023/24`,
+    year
+  )
+
+
+# ---- Save output to data/ folder ----
+usethis::use_data(lives_alcohol_misuse, overwrite = TRUE)
