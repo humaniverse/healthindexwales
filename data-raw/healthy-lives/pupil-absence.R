0093bb125c4b6b36ff65e2d80e14c0e3d94a8360
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-05-06 17:06:00 +0200

    Bump year code an simplify script

--- data-raw/healthy-lives/pupil-absence.R
+++ data-raw/healthy-lives/pupil-absence.R
@@ -5,12 +5,11 @@ library(geographr)
 
 # ---- Get and clean data ----
 # Wales LTLA and HB Codes
-wales_hb_ltla <- lookup_ltla21_lhb22
-
+wales_hb_ltla <- lookup_ltla21_lhb22 |>
+  select(ltla24_code = ltla21_code, ltla24_name = ltla21_name)
 
 # Primary School Absenteeism data
 # Source: https://www.gov.wales/absenteeism-primary-schools-september-2023-august-2024
-
 primary_absenteeism_link <- "https://www.gov.wales/sites/default/files/statistics-and-research/2024-12/absenteeism-from-primary-schools-september-2023-to-august-2024-649.ods"
 tempfile_p <- tempfile(fileext = ".ods")
 download.file(primary_absenteeism_link, destfile = tempfile_p, mode = "wb")
@@ -18,12 +17,14 @@ download.file(primary_absenteeism_link, destfile = tempfile_p, mode = "wb")
 primary_absenteeism_raw <- read_ods(tempfile_p, sheet = 10, skip = 3)
 
 primary_absenteeism <- primary_absenteeism_raw |>
-  filter(Measure == "Percentage of half-day sessions missed for unauthorised absence")
+  filter(
+    Measure == "Percentage of half-day sessions missed for unauthorised absence"
+  ) |>
+  select(ltla24_name = `Local authority`, primary_absence = `2023/24`)
 
 
 # Secondary School Absenteeism data
 # Source: https://www.gov.wales/absenteeism-secondary-schools-september-2023-august-2024
-
 secondary_absenteeism_link <- "https://www.gov.wales/sites/default/files/statistics-and-research/2024-09/absenteeism-from-secondary-schools-september-2023-to-august-2024.ods"
 tempfile_s <- tempfile(fileext = ".ods")
 download.file(secondary_absenteeism_link, destfile = tempfile_s, mode = "wb")
@@ -31,34 +32,28 @@ download.file(secondary_absenteeism_link, destfile = tempfile_s, mode = "wb")
 secondary_absenteeism_raw <- read_ods(tempfile_s, sheet = 10, skip = 3)
 
 secondary_absenteeism <- secondary_absenteeism_raw |>
-  filter(Attribute == "Percentage of half-day sessions missed for unauthorised absence")
-
+  filter(
+    Attribute ==
+      "Percentage of half-day sessions missed for unauthorised absence"
+  ) |>
+  select(ltla24_name = `Local authority`, secondary_absence = `2023/24`)
 
 # Join absenteeism data
 pupil_absenteeism <- primary_absenteeism |>
-  left_join(secondary_absenteeism, by = c("Local authority" = "Local authority")) |>
-  mutate(`Local authority` = str_remove(`Local authority`, "^The\\s+")) |>
-  select(`Local authority`,
-    primary_absence = `2023/24.x`,
-    secondary_absence = `2023/24.y`
-  )
-
+  left_join(secondary_absenteeism) |>
+  mutate(ltla24_name = str_remove(ltla24_name, "^The\\s+"))
 
 # Join absenteeism and geographic data
 lives_pupil_absence <- pupil_absenteeism |>
-  left_join(wales_hb_ltla, by = c("Local authority" = "ltla21_name")) |>
-  group_by(`Local authority`) |>
-  mutate(
-    total_pupil_absence_percentage = ((primary_absence + secondary_absence) / 2),
-    year = "2023/24"
+  left_join(wales_hb_ltla) |>
+  summarise(
+    total_pupil_absence_percentage = mean(c(
+      primary_absence,
+      secondary_absence
+    )),
+    .by = ltla24_code
   ) |>
-  ungroup() |>
-  select(
-    ltla23_code = ltla21_code,
-    total_pupil_absence_percentage,
-    year
-  )
-
+  mutate(year = "2023/24")
 
 # ---- Save output to data/ folder ----
 usethis::use_data(lives_pupil_absence, overwrite = TRUE)
